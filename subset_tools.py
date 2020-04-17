f092f03e0a05899e0233034ee18b9e6789473290
Ahmad Rezaii <ahmadrezaii@u.boisestate.edu>
2020-04-17 15:22:54 -0600

    add comments

--- subset_tools.py
+++ subset_tools.py
@@ -1,11 +1,13 @@
+import os
 
-class Conus():
+
+class Conus:
     """
     Class to hold the information about the CONUS dataset we are working with
     """
     def __init__(self, version, local_path):
-
-        if version == 1:
+        self.version = version
+        if self.version == 1:
             self.files = {
                 'CONUS_MASK': 'Domain_Blank_Mask.tif',
                 'SUBSURFACE_DATA': 'grid3d.v3.pfb',
@@ -15,7 +17,7 @@ class Conus():
             }
             self.cyverse_path = '/iplant/home/shared/avra/CONUS_1.0/SteadyState_Final/Other_Domain_Files/'
             self.local_path = local_path
-        if version == 2:
+        if self.version == 2:
             self.files = {
                 'CONUS_MASK': 'conus_1km_PFmask2.tif',
                 'SUBSURFACE_DATA': '3d-grid.v3.tif',
@@ -31,9 +33,31 @@ class Conus():
             }
             self.cyverse_path = '/iplant/home/shared/avra/CONUS2.0/Inputs/domain/'
             self.local_path = local_path
+        self.check_destination
+
+    def check_inputs_exist(self):
+        """
+        Look for each input file to see if it exists
+        :return: the list of missing input files
+        """
+        missing = []
+        for file in self.files.values():
+            if not os.path.isfile(os.path.join(self.local_path, file)):
+                missing.append(file)
+        return missing
+
+    @property
+    def check_destination(self):
+        """
+        make sure the local folder to store inputs exists
+        :return: True if folder exists, raises Exception if local destination folder not found
+        """
+        if not os.path.isdir(self.local_path):
+            raise Exception(f'Destination {self.local_path} for CONUS{self.version} input file does not exist')
+        return True
 
 
-class CyVerseDownloader():
+class CyVerseDownloader:
     from irods.session import iRODSSession
     from irods.exception import DataObjectDoesNotExist
     from pathlib import Path
@@ -47,11 +71,15 @@ class CyVerseDownloader():
         self.destination = dest
 
     def get_conus_data(self, file):
-        with self.iRODSSession(host='data.cyverse.org', port=1247, user=self.username, password=self.password, zone='iplant') as session:
-            obj = session.data_objects.get(file)
+        with self.iRODSSession(host='data.cyverse.org',
+                               port=1247,
+                               user=self.username,
+                               password=self.password,
+                               zone='iplant') as session:
             try:
+                obj = session.data_objects.get(file)
                 # open(self.Path(file).name, 'wb').write(obj.open().read())
-                with open(self.os.path.join(self.destination ,self.Path(file).name), 'wb') as f:
+                with open(self.os.path.join(self.destination, self.Path(file).name), 'wb') as f:
                     with obj.open() as f_obj:
                         f.write(f_obj.read())
             except self.DataObjectDoesNotExist:
@@ -59,11 +87,14 @@ class CyVerseDownloader():
 
 
 def main():
-    conus2 = Conus(2, 'CONUS2_Inputs')
+    #conus2 = Conus(2, 'CONUS2_Inputs')
     conus1 = Conus(1, 'CONUS1_Inputs')
-    cdl = CyVerseDownloader(dest=conus2.local_path)
-    for file in conus2.files.values():
-        cdl.get_conus_data(f'{conus2.cyverse_path}{file}')
+    cdl = CyVerseDownloader(dest=conus1.local_path)
+    for file in conus1.files.values():
+        cdl.get_conus_data(f'{conus1.cyverse_path}{file}')
+    missing_files = conus1.check_inputs_exist()
+    if len(missing_files) > 0:
+        print(f'Missing the following files {missing_files}')
 
 
 if __name__ == '__main__':
--- subset_tools_tests.py
+++ subset_tools_tests.py
@@ -1,19 +1,34 @@
 import unittest
-from subset_tools import CyVerseDownloader
+from subset_tools import CyVerseDownloader, Conus
 import os
+import shutil
 
 
 class CyVerseDownloaderTests(unittest.TestCase):
+    readme_filesize = 2680
+
     def test_connect(self):
         user = os.environ.get('CYVERSE_USERNAME')
         password = os.environ.get('CYVERSE_PASSWORD')
         cs = CyVerseDownloader(user, password)
         cs.get_conus_data('/iplant/home/shared/avra/CONUS_1.0/SteadyState_Final/Other_Domain_Files/ReadMe_Files.txt')
-        self.assertEqual(2680, os.path.getsize('ReadMe_Files.txt'))
+        self.assertEqual(self.readme_filesize, os.path.getsize('ReadMe_Files.txt'))
 
-# class MyTestCase(unittest.TestCase):
-#     def test_something(self):
-#         self.assertEqual(True, False)
+
+class ConusClassTests(unittest.TestCase):
+    testDir = 'TestDirectory'
+
+    def test_folder_exists(self):
+        os.mkdir(self.testDir)
+        try:
+            Conus(1, self.testDir)
+        except:
+            self.fail("Conus(1, self.testDir) raised an exception unexpectedly!")
+        shutil.rmtree(self.testDir)
+
+    def test_folder_not_exists(self):
+        with self.assertRaises(Exception):
+            Conus(1, self.testDir)
 
 
 if __name__ == '__main__':
