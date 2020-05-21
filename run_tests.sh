a52f74100ba9cfec1d38143dbd57392ba6d8f9ac
Ahmad Rezaii <ahmadrezaii@u.boisestate.edu>
2020-05-20 22:17:58 -0600

    update test run script

--- run_tests.sh
+++ run_tests.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
-
-python -m unittest discover . -p '*_tests.py'
+if [ $# -eq 2 ]
+  then
+    export CYVERSE_USERNAME=$1
+    export CYVERSE_PASSWORD=$2
+else
+  echo "Warning: Cyverse Downloader tests will fail if username/password not supplied"
+  echo "example: ./run_tests.sh <cyverse_username> <cyverse_password>"
+fi
+python -m unittest discover test -p '*_tests.py'
