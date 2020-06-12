5884b0ad6815ceaf1221fb7ca7a5cebc4b782930
Ahmad Rezaii <ahmadrezaii@u.boisestate.edu>
2020-06-12 13:52:02 -0600

    ignore clm tests if latlon file not found

--- tests/clm_utils_tests.py
+++ tests/clm_utils_tests.py
@@ -13,7 +13,7 @@ class ClmUtilsClipperRegressionTests(unittest.TestCase):
     def test_clm_clip_land_cover(self):
         if os.environ.get('TRAVIS'):
             pass
-        else:
+        elif os.path.isfile(test_files.conus1_latlon):
             mask_array = file_io_tools.read_file(test_files.huc10190004.get('conus1_mask'))
             ds_ref = file_io_tools.read_geotiff(test_files.conus1_mask)
             clm_clipper = ClmClipper(mask_array, ds_ref)
@@ -27,11 +27,15 @@ class ClmUtilsClipperRegressionTests(unittest.TestCase):
                                      'Writing vegm file matches reference for conus1')
 
             os.remove('WBDHU8_vegm_test.dat')
+        else:
+            print('WARNING! Unable to run test test_clm_clip_latlon because source file not found. '
+                  'copy conus1_Grid_Centers_Short_Deg.format.sa into test_inputs/CONUS1_Inputs to enable test!')
+            pass
 
     def test_clm_clip_latlon(self):
         if os.environ.get('TRAVIS'):
             pass
-        else:
+        elif os.path.isfile(test_files.conus1_latlon):
             mask_array = file_io_tools.read_file(test_files.huc10190004.get('conus1_mask'))
             ds_ref = file_io_tools.read_geotiff(test_files.conus1_mask)
             clm_clipper = ClmClipper(mask_array, ds_ref)
@@ -39,9 +43,14 @@ class ClmUtilsClipperRegressionTests(unittest.TestCase):
             clm_clipper.write_lat_lon(latlon_formatted, 'WBDHU8_latlon_test.sa', x=latlon_data.shape[2],
                                       y=latlon_data.shape[1], z=latlon_data.shape[0])
             self.assertIsNone(np.testing.assert_array_equal(file_io_tools.read_file('WBDHU8_latlon_test.sa'),
-                                                            file_io_tools.read_file('test_inputs/WBDHU8_latlon_orig.sa')),
+                                                            file_io_tools.read_file(
+                                                                'test_inputs/WBDHU8_latlon_orig.sa')),
                               'writing and reading a tif gives back the same array values')
             os.remove('WBDHU8_latlon_test.sa')
+        else:
+            print('WARNING! Unable to run test test_clm_clip_latlon because source file not found. '
+                  'copy conus1_Grid_Centers_Short_Deg.format.sa into test_inputs/CONUS1_Inputs to enable test!')
+            pass
 
 
 if __name__ == '__main__':
